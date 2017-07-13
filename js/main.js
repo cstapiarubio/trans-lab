7032c2ed6c1eb63ddbc653a7927f4a782930f6b9
cstapiarubio <cstapiarubio@gmail.com>
2017-07-13 13:17:08 -0400

    validacion email

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 /* Holiii acá va tu código también */
+
+$(document).ready(function() {
+	/* validación de correo*/
+	$('#inicio').click(function() {
+    // Expresion regular para validar el correo
+    var regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
+
+    // Se utiliza la funcion test() nativa de JavaScript
+    if (regex.test($('#email').val().trim())) {
+    	alert('Correo validado');
+    } else {
+    	alert('La dirección de correo no es válida');
+    }
+   });
+});
+
